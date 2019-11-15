d22d9269ca7b9b98675b6e906b5320fa2daa0c19
Luke Garceau <luke.garceau@kavokerr.com>
2019-11-15 10:03:22 -0500

    update

--- src/util/hexToEng.py
+++ src/util/hexToEng.py
@@ -5,6 +5,36 @@ from util import rotatingLogger as logger, hygieneData
 config = configparser.ConfigParser()
 config.read(const.CONFIG_PATH)
 
+## need to create fucntion to handle this and then continue testing later tonight
+def handleFile(file='none'):
+    dataDir = config.get('Paths', 'dataPath')
+    filePath = os.path.join(dataDir, file)
+    operation = 'a'
+    if os.path.stat(filePath).ST_SIZE >= config.get('Size', 'maxCsvSize') or file == 'none':
+        os.system('mv %s /home/User1/out/%s' % (filePath, file))
+        operation = 'w'
+        fileName = socket.gethostname()
+        fileName = fileName+'_'
+        timestamp = datetime.datetime.now()
+        timestamp = '%s%s%s_%s%s%s' % (timestamp.day, timestamp.month, timestamp.year, timestamp.hour, timestamp.minute, timestamp.second)
+        fileName = fileName+timestamp+'.csv'
+        file = fileName
+    filePath = os.path.join(dataDir, file)
+    return filePath, operation
+
+def record(data):
+    # wait for program to remove other csvs
+    const.CAN_DATA_FILE = handleFile()[0]
+        
+    removes = []
+    logger.get_logger().info('---------------: '+str(data))
+    
+    (fileName, opertation) = handleFile(const.CAN_DATA_FILE)
+    with open(fileName, opertation) as file:
+        file.write('%s,ID:\t%sMessage:\t%s' % (data[0], data[1], data[2]))
+        file.close()
+
+
 @asyncio.coroutine
 async def interpret():
     compTimer = time.time()/60
@@ -116,8 +146,9 @@ async def interpret():
                         messages.append([timestamp, ['timeType', 'idleTime'], ['timeAmt', str(idleTimeInt/60.0)]])
                 elif whichTime == use:
                     # useTime
+                    
+                    logger.get_logger().info('useCode: '+str(data))
                     record(data)
-                    #logger.get_logger().info('useCode: '+str(data))
                     #logger.get_logger().info('msgToRecord: '+str(const.MSG_TO_RECORD))
                     if deviceState == 'idle':
                         useTimeLast = compTimer
@@ -353,31 +384,3 @@ def getErrorMessage(canMessage):
         
     return None, False
 
-def record(data):
-    # wait for program to remove other csvs
-    const.CAN_DATA_FILE = handleFile()[0]
-        
-    removes = []
-    logger.get_logger().info('---------------: '+str(data))
-    
-    (fileName, opertation) = handleFile(const.CAN_DATA_FILE)
-    with open(fileName, opertation) as file:
-        file.write('%s,ID:\t%sMessage:\t%s' % (data[0], data[1], data[2]))
-        file.close()
-
-## need to create fucntion to handle this and then continue testing later tonight
-def handleFile(file='none'):
-    dataDir = config.get('Paths', 'dataPath')
-    filePath = os.path.join(dataDir, file)
-    operation = 'a'
-    if os.path.stat(filePath).ST_SIZE >= config.get('Size', 'maxCsvSize') or file == 'none':
-        os.system('mv %s /home/User1/out/%s' % (filePath, file))
-        operation = 'w'
-        fileName = socket.gethostname()
-        fileName = fileName+'_'
-        timestamp = datetime.datetime.now()
-        timestamp = '%s%s%s_%s%s%s' % (timestamp.day, timestamp.month, timestamp.year, timestamp.hour, timestamp.minute, timestamp.second)
-        fileName = fileName+timestamp+'.csv'
-        file = fileName
-    filePath = os.path.join(dataDir, file)
-    return filePath, operation
